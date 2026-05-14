ee60c578b3708b4da825e2f52933ee4adbdc37f3
hilal-2025 <24200675@siswa.um.edu.my>
2026-05-14 21:16:56 +0800

    (Hilal) Added ADT Comment and pput some method in smartLibrary to remove the errors

--- src/LibraryADT.java
+++ src/LibraryADT.java
@@ -1,9 +1,13 @@
 public interface LibraryADT {
+    // Adds a new book to the library
     void addBook(int isbn, String title, String author);
     
+    // Returns the Book object if found, otherwise returns null
     Book SearchBook(int isbn);
 
+    // Allows a user to borrow a book by its ISBN
     void borrowBook(int isbn);
 
+    // Allows a user to view history of borrowed books
     void viewLatestHistory();
 }
--- src/SmartLibrary.java
+++ src/SmartLibrary.java
@@ -1,3 +1,27 @@
-public class SmartLibrary {
+public class SmartLibrary implements LibraryADT {
     //some logic
+
+    // Adds a new book to the library
+    @Override
+    public void addBook(int isbn, String title, String author){
+        System.out.println("Book with ISBN " + isbn + " has been added to the library.");
+    }
+    
+    // Returns the Book object if found, otherwise returns null
+    @Override
+    public Book SearchBook(int isbn){
+        return null;
+    }
+
+    // Allows a user to borrow a book by its ISBN
+    @Override
+    public void borrowBook(int isbn){
+        System.out.println("Book with ISBN " + isbn + " has been borrowed.");
+    }
+
+    // Allows a user to view history of borrowed books
+    @Override
+    public void viewLatestHistory(){
+        System.out.println("Displaying the latest history of borrowed books.");
+    }
 }
